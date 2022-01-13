d4efa78c0cf0ec484f8a951e8a1809b9888d282b
Gorm Reventlow <gorm@reventlow.com>
2022-01-13 20:37:59 +0100

    search excel file

--- infoscreen.py
+++ infoscreen.py
@@ -10,17 +10,23 @@ from zipfile import ZipFile
 import os
 import time
 import socket
+import xlrd
+
+excelfile_infoscreen_info = "data.xls"
 
 
 class ComputerInfo:
 
-    def __init__(self, os, macaddress, ip):
+    def __init__(self, hostname:str, os:str, macaddress:str, ip:str):
         self.macaddress = macaddress
         self.ip = ip
         self.os = os
+        self.hostname = hostname
+
+    def get_hostname(self):
+        return self.hostname
 
     def get_macadress(self):
-
         return self.macaddress
 
     def get_ip(self):
@@ -53,8 +59,8 @@ def start_webdriver(this_platform):
     return browser
 
 
-def win_download_and_unzip(chromedriver_version_url: str, extract_to='.'):
-    download_url: str
+def win_download_and_unzip(chromedriver_version_url:str, extract_to='.'):
+    download_url:str
 
     file = urllib.request.urlopen(chromedriver_version_url)
     for line in file:
@@ -84,18 +90,60 @@ def start_webdriver():
     browser.get(get_url())
     time.sleep(900)
 
+def excel_infoscreen_get_sharepoint_screen(macAddress: str):
+
+    wb = xlrd.open_workbook(excelfile_infoscreen_info)
+    sheet = wb.sheet_by_index(0)
+    sheet.cell_value(0, 0)
+
+    item_found = False
+    this_hostname = ""
+    this_os = ""
+    this_macAddress= ""
+    this_ip = ""
+
+    for i in range(sheet.nrows):
+        if macAddress in sheet.cell_value(i, 2):
+
+            item_found = True
+
+            this_hostname = sheet.cell_value(i, 0)
+            this_os = sheet.cell_value(i, 1)
+            this_macAddress = sheet.cell_value(i, 2)
+            this_ip = sheet.cell_value(i, 3)
+
+            if this_ip != C.get_ip():
+                print("ip_error")
+            else:
+                print("ip_correct")
+
+
+        if item_found == False:
+            print("mac_correct")
+        else:
+            print("mac_error")
+
+    return this_hostname, this_os, this_macAddress, this_ip
+
+
 def main():
     macaddress = hex(uuid.getnode())
     macaddress = ':'.join(a + b for a, b in zip(macaddress[::2], macaddress[1::2]))
+
+    macaddress = str(macaddress)
     ip = socket.gethostbyname(socket.gethostname())
     os = platform.system()
+    hostname = socket.gethostname()
 
+    global C
+    C = ComputerInfo(hostname, os, macaddress, ip)
 
-    c = ComputerInfo(os, macaddress, ip)
+    print(C.get_hostname())
+    print(C.get_os())
+    print(C.get_macadress())
+    print(C.get_ip())
+    #excelfile_infoscreen_info(macaddress)
 
-    print(c.get_os())
-    print(c.get_macadress())
-    print(c.get_ip())
 
 
 if __name__ == "__main__":
